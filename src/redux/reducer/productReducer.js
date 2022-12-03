842ffc68c421bff32ab0fc386ee0947df157f135
AnikaLabiba <anikalabibajahin@gmail.com>
2022-12-03 18:53:28 +0600

    add to cart implemented

---
+++ src/redux/reducer/productReducer.js
@@ -0,0 +1,16 @@
+import { ADD_TO_CART } from '../actionTypes/actionTypes'
+
+export const initialState = {
+    cart: []
+};
+
+export const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, action.payload]
+            }
+        default: return state
+    }
+}
